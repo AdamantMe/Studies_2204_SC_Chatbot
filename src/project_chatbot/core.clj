5eb2197497f82af329369f880982dc52ba95d9a2
Ali Ghaedi <ali.ghaedi598@gmail.com>
2022-10-31 12:35:04 +0100

    testing

--- src/project_chatbot/core.clj
+++ src/project_chatbot/core.clj
@@ -3,3 +3,7 @@
 
 (defn -main [& args]
   (println "Testing..."))
+
+(def parks-info
+  {"bertramka" "Bertramka is a park with a garden which was often the home to W. A. Mozart, who's museum you can visit there. It is open all year long."
+   })
