30877c491d34af0fc395dcbcbd983e4925a93a47
Ali Ghaedi <ali.ghaedi598@gmail.com>
2022-12-02 12:43:21 +0100

    code clean

--- src/project_chatbot/process_input.clj
+++ src/project_chatbot/process_input.clj
@@ -5,10 +5,10 @@
 (defn process_input_string [input_string]
   ;; (println "Processing input...")
   (data/last-query-set-park :Bertramka) ;; Temporary hardcoded, only for the prototype
-  (data/last-query-set-activity (data/get-synonym-from-keyword (map symbol
-                                                                    (re-seq #"ride|riding|bike|biking|skate|skating|sport|sports|play|playing|playground|get to|get from|transport|transportation|parking|park a car|wc|toilet|restroom|dog|dogs|attractions|interesting|things to do|food|eat|restaurant|ski|skiing" input_string))))
-  ;; (string/replace input_string #"x" "modified")
-  )
+
+  (let [input_keywords (map symbol (re-seq #"ride|riding|bike|biking|skate|skating|sport|sports|play|playing|playground|get to|get from|transport|transportation|parking|park a car|wc|toilet|restroom|dog|dogs|attractions|interesting|things to do|food|eat|restaurant|ski|skiing" input_string))]
+
+    (data/last-query-set-activity (data/get-synonym-from-keyword input_keywords))))
 
 
 
