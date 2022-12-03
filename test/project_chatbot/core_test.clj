1e300e92b56a4e0641d1841afc5dcf9d3dca327d
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-12-03 14:24:43 +0100

    Testing added

--- test/project_chatbot/core_test.clj
+++ test/project_chatbot/core_test.clj
@@ -1,7 +1,8 @@
 (ns project-chatbot.core-test
   (:require [clojure.test :refer :all]
-            [project-chatbot.core :refer :all]))
+            [project-chatbot.core :refer :all]
+            [project-chatbot.data-ops :as data]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-get-synonym-from-keyword-1
+  (testing "Can not find a correct synonym for the keyword"
+    (is (= (data/get-synonym-from-keyword '(ride)) :biking))))
