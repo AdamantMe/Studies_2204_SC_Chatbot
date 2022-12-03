da3df8ad4d46faa5b5b1016bb017f7bf2cb5aef3
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-12-03 15:45:51 +0100

    greeting on startup

--- src/project_chatbot/core.clj
+++ src/project_chatbot/core.clj
@@ -5,6 +5,11 @@
   (:gen-class))
 
 (defn chatbot-init []
+  
+  (println "Hello, please enter your name?")
+  (def username (read-line))
+  (println "Hello" username", please ask me a question about parks in Prague.") 
+  
   (loop [last_input (read-line)]
     (if (not (nil? (input/process_input_string last_input)))
       (output/process_output_string)
