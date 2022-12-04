a294aa5263a8fe00ee40c71751aabfc43f4584f9
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-12-04 16:28:59 +0100

    More testing

--- test/project_chatbot/core_test.clj
+++ test/project_chatbot/core_test.clj
@@ -6,3 +6,27 @@
 (deftest test-get-synonym-from-keyword-1
   (testing "Can not find a correct synonym for the keyword"
     (is (= (data/get-synonym-from-keyword '(ride)) :biking))))
+
+(ns project-chatbot.core-test
+  (:require [clojure.test :refer :all]
+            [project-chatbot.core :refer :all]
+            [project-chatbot.data-ops :as data]))
+
+(def test_keyword_long
+  (map symbol (re-seq #"get to" "get to")))
+
+(deftest test-get-synonym-from-keyword-1
+  (testing "Can not find a correct synonym for the keyword"
+    (is (= (data/get-synonym-from-keyword '(ride)) :biking))))
+
+(deftest test-get-synonym-from-keyword-2
+  (testing "Can not find a correct synonym for the keyword"
+    (is (= (data/get-synonym-from-keyword test_keyword_long) :transportation))))
+
+(deftest test-get-activity-availability-1
+  (testing "Error receiving activity's availability from the JSON file"
+    (is (= (data/get-activity-availability :nonexistent) nil))))
+
+(deftest test-get-activity-availability-2
+  (testing "Error receiving activity's availability from the JSON file"
+    (is (= (data/get-activity-availability :biking) true))))
