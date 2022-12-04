6180b42b0d9f7aba6e568e352abbbb160a81605d
tamahhamat <98357200+tamahhamat@users.noreply.github.com>
2022-12-04 16:41:48 +0100

    fixed testing

--- test/project_chatbot/core_test.clj
+++ test/project_chatbot/core_test.clj
@@ -5,15 +5,6 @@
 
 (data/last-query-set-park :bertramka)
 
-(deftest test-get-synonym-from-keyword-1
-  (testing "Can not find a correct synonym for the keyword"
-    (is (= (data/get-synonym-from-keyword '(ride)) :biking))))
-
-(ns project-chatbot.core-test
-  (:require [clojure.test :refer :all]
-            [project-chatbot.core :refer :all]
-            [project-chatbot.data-ops :as data]))
-
 (def test_keyword_long
   (map symbol (re-seq #"get to" "get to")))
 
