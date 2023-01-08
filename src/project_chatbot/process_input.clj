3f2a5ce80200da0443c97b0e1e19580d917f2f06
Ali Ghaedi <ali.ghaedi598@gmail.com>
2023-01-08 20:35:02 +0100

    minor fix

--- src/project_chatbot/process_input.clj
+++ src/project_chatbot/process_input.clj
@@ -15,8 +15,7 @@
       nil))
 
   (let [input_keywords (map symbol (re-seq #"ride|riding|bike|biking|skate|skating|sport|sports|soccer|football|frisbee|play|playing|playground|get to|get from|get there|transport|transportation|parking|park a car|wc|toilet|restroom|dog|dogs|attractions|interesting|things to do|food|to eat|restaurant|ski|skiing" input_string))]
-
     (if (not (empty? input_keywords))
       ;; Once correct activity is identified and made "JSON friendly", set it as user's "Last Querry"'s activity to access it easier later on.
-      (data/last-query-set-activity (data/get-synonym-from-keyword input_keywords))
+      (data/last-query-set-activity (data/get-activity-synonym-from-keyword input_keywords))
       nil)))
